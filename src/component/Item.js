0ab773371b5171d7253921be64d479daf94f1d2d
Leqi <michaelleqihust@gmail.com>
2020-08-14 15:45:24 +0800

    [leqi] feat: finish store home page design.

---
+++ src/component/Item.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../style/item.less';
+
+class Item extends React.Component {
+  render() {
+    let itemData = this.props.itemData;
+    return (
+      <div className={'item_div'}>
+        <img src={itemData.itemUrl} alt={'item image'}/>
+        <label className={'item_name_label'}>{itemData.itemName}</label>
+        <label className={'item_price_label'}>单价: {itemData.itemPrice} 元 / {itemData.itemUnit}</label>
+        <button className={'add_item_to_order_button'}>+</button>
+      </div>
+    );
+  }
+}
+
+export default Item;
--- src/component/ShopStore.js
+++ src/component/ShopStore.js
@@ -1,9 +1,47 @@
 import React from "react";
+import Item from "./Item";
 
 class ShopStore extends React.Component {
+  state = {
+    items: [
+      {
+        itemName: 'cola1',
+        itemPrice: 1,
+        itemUnit: '瓶',
+        itemUrl: 'https://d2wvksc3i53c0p.cloudfront.net/media/catalog/product/cache/2/image/85e4522595efc69f496374d01ef2bf13/1/1/11777.jpg',
+      },
+      {
+        itemName: 'cola2',
+        itemPrice: 1,
+        itemUnit: '瓶',
+        itemUrl: 'https://d2wvksc3i53c0p.cloudfront.net/media/catalog/product/cache/2/image/85e4522595efc69f496374d01ef2bf13/1/1/11777.jpg',
+      },
+      {
+        itemName: 'cola3',
+        itemPrice: 1,
+        itemUnit: '瓶',
+        itemUrl: 'https://d2wvksc3i53c0p.cloudfront.net/media/catalog/product/cache/2/image/85e4522595efc69f496374d01ef2bf13/1/1/11777.jpg',
+      },
+      {
+        itemName: 'cola4',
+        itemPrice: 1,
+        itemUnit: '瓶',
+        itemUrl: 'https://d2wvksc3i53c0p.cloudfront.net/media/catalog/product/cache/2/image/85e4522595efc69f496374d01ef2bf13/1/1/11777.jpg',
+      },
+    ],
+  }
+
   render() {
     return (
-      <h1>shop store</h1>
+      <div className={'shop_store_div'}>
+        <div className={'shop_store_display_div'}>
+          {this.state.items.map(item => {
+            return (
+              <Item key={item.itemName} itemData={item}/>
+            );
+          })}
+        </div>
+      </div>
     );
   }
 }
